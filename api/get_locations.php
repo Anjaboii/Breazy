bed79d24b902c6064087c334ee1928bb6f2573a0
ANJANA HERATH <vinukthiherath@gmail.com>
2025-03-30 14:48:42 +0530

    5

--- api/get_locations.php
+++
@@ -1,6 +0,0 @@
-<?php
-require '../config/db.php';
-header('Content-Type: application/json');
-
-$locations = $conn->query("SELECT * FROM custom_locations");
-echo json_encode($locations->fetch_all(MYSQLI_ASSOC));
--- api/save_location.php
+++
@@ -1,5 +0,0 @@
-session_start();
-if (!isset($_SESSION['admin_logged_in'])) {
-    http_response_code(403);
-    die('Unauthorized');
-}
